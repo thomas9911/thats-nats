bc4e15e32019f51e2dad4dc86e21e19ab4a36654
Thomas Timmer <thomastimmer11@hotmail.com>
2024-04-01 19:09:27 +0200

    feat: let run also use a service

--- services/thats-nats-rs/src/main.rs
+++ services/thats-nats-rs/src/main.rs
@@ -1,4 +1,4 @@
-use async_nats::jetstream::consumer;
+// use async_nats::jetstream::consumer;
 use futures_util::StreamExt;
 
 // const STREAM_NAME: &str = "testing";
@@ -38,32 +38,74 @@ use futures_util::StreamExt;
 //     Ok(())
 // }
 
+// use rhai::Engine;
+// use rhai::packages::Package;
+// use rhai_rand::RandomPackage;
+
+// const CHANNEL_NAME: &str = "rust";
+
+// #[tokio::main]
+// async fn main() -> anyhow::Result<()> {
+//     // let nc = async_nats::connect("nats://127.0.0.1:4222").await?;
+//     let nc = async_nats::ConnectOptions::new()
+//     .no_echo()
+//     .connect("nats://127.0.0.1:4222")
+//     .await?;
+
+//     let mut messages = nc.subscribe(CHANNEL_NAME).await?;
+//     let mut engine = Engine::new();
+//     let random = RandomPackage::new();
+//     random.register_into_engine(&mut engine);
+
+//     while let Some(message) = messages.next().await {
+//         println!("got message {:?}", message);
+
+//         if let Some(reply) = &message.reply {
+//             let payload = std::str::from_utf8(&message.payload)?;
+//             let out = match engine.eval::<rhai::Dynamic>(&payload){
+//                 Ok(res) => format!(r#"{{"result": {}}}"#, serde_json::to_string(&res)?),
+//                 Err(err) => format!(r#"{{"error": "{}"}}"#, err.to_string()),
+//             };
+//             nc.publish(reply.as_str().to_string(), out.into()).await?;
+//         }
+//     }
+
+//     Ok(())
+// }
+
+use rhai::packages::Package;
 use rhai::Engine;
-use rhai::packages::Package; 
 use rhai_rand::RandomPackage;
 
-const CHANNEL_NAME: &str = "rust";
-
+use async_nats::service::ServiceExt;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // let nc = async_nats::connect("nats://127.0.0.1:4222").await?;
     let nc = async_nats::ConnectOptions::new()
-    .no_echo()
-    .connect("nats://127.0.0.1:4222")
-    .await?;
+        .no_echo()
+        .connect("nats://127.0.0.1:4222")
+        .await?;
 
-    let mut messages = nc.subscribe(CHANNEL_NAME).await?;
+    let service = nc
+        .service_builder()
+        .description("run a rhai script")
+        .start("rhai_aas", "0.1.0")
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+    let mut endpoint = service
+        .group("rhai")
+        .endpoint("run")
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
     let mut engine = Engine::new();
     let random = RandomPackage::new();
     random.register_into_engine(&mut engine);
 
-    while let Some(message) = messages.next().await {
-        println!("got message {:?}", message);
-
-        if let Some(reply) = &message.reply {
-            let payload = std::str::from_utf8(&message.payload)?;
-            let out = match engine.eval::<rhai::Dynamic>(&payload){
+    while let Some(request) = endpoint.next().await {
+        if let Some(reply) = &request.message.reply {
+            let payload = std::str::from_utf8(&request.message.payload)?;
+            let out = match engine.eval::<rhai::Dynamic>(&payload) {
                 Ok(res) => format!(r#"{{"result": {}}}"#, serde_json::to_string(&res)?),
                 Err(err) => format!(r#"{{"error": "{}"}}"#, err.to_string()),
             };
--- test.sh
+++ test.sh
@@ -4,7 +4,11 @@ inner_nats() {
     ./nats "$@"
 }
 
+# list found microservices
+inner_nats micro list
+
 # test against elixir service
 inner_nats request 'elixir.run' ':rand.bytes(32) |> Base.encode32(padding: false)'
 
-inner_nats request 'rust' 'rand_float(0.0, 12345.0)'
+# test against rust service that runs rhai script
+inner_nats request 'rhai.run' 'rand_float(0.0, 12345.0)'
